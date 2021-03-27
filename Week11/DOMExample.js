c3786e76effaa5237f9111f6644541a6699f2c37
Kraucon01 <kraucon01@gmail.com>
2021-03-26 21:58:48 -0400

    My progress for this assignment

--- Week11/DOMExample.js
+++ Week11/DOMExample.js
@@ -1,14 +1,21 @@
 function checkEmail(){
     var emailName = evl.value;
     if(emailName.length <3){
-        evlMsg.className=''
+        evlMsg.className='warning';
+        evlMsg.textContent='This is not long enough, yet...';
+    }else{
+        evlMsg.textContent='';
     }
-
 }
 
+function tipEmail(){
+    evlMsg.className = 'tip';
+    evlMsg.innerHTML = 'Email must be at least 5 characters long'
+}
 
 var evl = document.getElementById('ContactName')
 var evlMsg = document.getElementById('messageInfo')
 
 //adding event listeners
-evl.addEventListener('focus', )
+evl.addEventListener('focus', tipEmail, false);
+evl.addEventListener('blur', checkEmail, false);
